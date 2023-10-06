c5617f14c368e37ca15d11f4536e72942d61178a
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-06 11:34:39 +0530

    Update spell-check.js

--- spell-check.js
+++ spell-check.js
@@ -1,18 +1,30 @@
+const fs = require('fs');
 const spellChecker = require('spellchecker');
-const exceptions = ["bic", "datetime"];
 
-const codeStyleRegex = /[_]/; // snake_case
+function findSpellingMistakesInYamlFile(filePath) {
+  // Read the content of the YAML file
+  const yamlContent = fs.readFileSync(filePath, 'utf8');
 
-module.exports = (input) => {
-  const words = input.split(codeStyleRegex);
-  const mistakes = words
-    .filter((word) => !exceptions.includes(word))
-    .filter((word) => spellChecker.isMisspelled(word));
+  // Split the content into words (split by spaces, newlines, etc.)
+  const words = yamlContent.split(/\s+/);
 
-  if (mistakes.length > 0) {
-    return [{
-      message: `Spelling mistakes found: ${mistakes.join(', ')}`,
-    }];
-  }
-  return [];
+  // Filter out any words that should be excluded from spell checking
+  const exceptions = ["bic", "datetime", "gt", "gte", "icontains", "iban", "idempotency", "isnull", "lt", "lte", "md5", "mimetype", "oid", "userpic"];
+  const filteredWords = words.filter((word) => !exceptions.includes(word.toLowerCase()));
+
+  // Find spelling mistakes
+  const mistakes = filteredWords.filter((word) => spellChecker.isMisspelled(word));
+
+  return mistakes;
+}
+
+// Example usage:
+const filePath = 'code/API_definitions/qod-api.yaml';
+const spellingMistakes = findSpellingMistakesInYamlFile(filePath);
+
+if (spellingMistakes.length > 0) {
+  console.log('Spelling mistakes found:');
+  console.log(spellingMistakes);
+} else {
+  console.log('No spelling mistakes found.');
 }
