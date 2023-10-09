3b8fb57ea2445c992f3c1909ea05f04f43bbe740
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-09 12:23:27 +0530

    Update spell-check.js

--- spell-check.js
+++ spell-check.js
@@ -4,19 +4,19 @@ const spellChecker = require('spellchecker');
 function findSpellingMistakesInYamlFile(filePath) {
   // Read the content of the YAML file
   const yamlContent = fs.readFileSync(filePath, 'utf8');
- console.log(yamlContent);
+ console.log("YAML "+ yamlContent);
   // Split the content into words (split by spaces, newlines, etc.)
   const words = yamlContent.split(/\s+/);
-  console.log(words);
+  console.log("Words "+words);
 
   // Filter out any words that should be excluded from spell checking
   const exceptions = ["bic", "datetime", "gt", "gte", "icontains", "iban", "idempotency", "isnull", "lt", "lte", "md5", "mimetype", "oid", "userpic"];
   const filteredWords = words.filter((word) => !exceptions.includes(word.toLowerCase()));
-  console.log("Filterword" + filteredWords);
+  console.log("Filterword " + filteredWords);
 
   // Find spelling mistakes
   const mistakes = filteredWords.filter((word) => spellChecker.isMisspelled(word));
-  console.log(mistakes);
+  console.log("Mistake "+mistakes);
 
   return mistakes;
 }
@@ -27,7 +27,7 @@ const spellingMistakes = findSpellingMistakesInYamlFile(filePath);
 
 if (spellingMistakes.length > 0) {
   console.log('Spelling mistakes found:');
-  console.log(spellingMistakes);
+  console.log("Spelling Mistake  "+spellingMistakes);
 } else {
   console.log('No spelling mistakes found.');
 }
