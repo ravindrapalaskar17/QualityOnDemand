d0bdd9515f41ff6d947380dd3f7f491c35d3b2fa
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-10 12:27:40 +0530

    Update spell-check.js

--- spell-check.js
+++ spell-check.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
-const spellChecker = require('spellChecker');
+const spellChecker = require('spellchecker');
 
 function findSpellingMistakesInYamlFile(filePath) {
+  
   // Read the content of the YAML file
   const yamlContent = fs.readFileSync(filePath, 'utf8');
  console.log("YAML "+ yamlContent);
+  
   // Split the content into words (split by spaces, newlines, etc.)
   const words = yamlContent.split(/\s+/);
   console.log("Words "+words);
 
   // Filter out any words that should be excluded from spell checking
-  const exceptions = ["bic", "datetime", "gt", "gte", "icontains", "iban", "idempotency", "isnull", "lt", "lte", "md5", "mimetype", "oid", "userpic"];
+  const exceptions = ["MACE", "API"];
+  
   const filteredWords = words.filter((word) => !exceptions.includes(word.toLowerCase()));
   console.log("Filterword " + filteredWords);
 
