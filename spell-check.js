1d740fce7ccbd2e76800340439d569e3470bd9db
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-10 23:03:58 +0530

    Update spell-check.js

--- spell-check.js
+++ spell-check.js
@@ -1,73 +1,34 @@
-const fs = require('fs');
-const dictionary = require('dictionary-en');
-const nspell = require('nspell');
+var dictionary = require('dictionary-en')
+var nspell = require('nspell')
+var exceptions = ['eventId', 'eventType', 'eventTime', 'eventSubscriptionId', 'publicAddress', 'subnet', 'privateAddress', 'publicPort', 'sessionId', 'UUID', 'devicePorts', 'QoS', 'qosProfile', 'TCP', 'UDP', 'QOS_S', 'QOS_M', 'QOS_L', 'QOS_E', 'webhook', 'notificationUrl', 'notificationAuthToken', 'startedAt', 'expiresAt', 'qosprofiles', 'minDuration', 'maxDuration', 'packetDelayBudget', 'oneway', 'endtoend', 'jitter', 'roundtrip', 'ITU', 'eg', 'realtime', 'packetErrorLossRate', 'QCI', 'maxDownstreamRate', 'QOS_STATUS_CHANGED', 'qosStatus', 'statusInfo', 'DURATION_EXPIRED', 'Enduser', 'IoT', 'sensorsactuators', 'phoneNumber', 'networkAccessIdentifier', 'MNO', 'invoker', 'MNOs', 'MSISDN', 'GPSI', 'IdentifierDomain', 'DNS', 'ie', 'applicationServerPorts', 'maxDownstreamBurstRate', 'maxUpstreamRate', 'QoD', 'cmunication', 'QualityOnDemand', 'Telco', 'indepth', 'Telecom', 'VRGaming', 'backend', 'OverviewhttpsrawgithubusercontentcomcamaraprojectQualityOnDemandmaindocumentationAPI_documentationresourcesQoD_latency_overviewPNG', 'QOD', 'OAuth', 'andor', 'AppFlow', 'portranges', 'AppFlows', 'portportranges', 'Appflow', 'br', 'APIhttpsrawgithubusercontentcomcamaraprojectQualityOnDemandmaindocumentationAPI_documentationresourcesQoD_detailsPNG', 'CAMARA', 'DRAFThttpsgithubcomcamaraprojectQualityOnDemandblobmaindocumentationAPI_documentationQoSProfile_Mapping_Tablemd', 'IETF', 'addressmask', 'applicationServer', 'dottedquad', 'sessionssessionId', 'createSession', 'targetMinUpstreamRate', 'SessionId', 'SessionInfo', 'EventNotification', 'PhoneNumber', 'QosStatus', 'EventQosStatus', 'ErrorInfo', 'GBR', 'latencysensitive', 'DOCSIS', 'maxUpstreamBurstRate', 'targetMinDownstreamRate', 'qosprofilesname', 'RateUnitEnum', 'CreateSession', 'PortsSpec', 'QosProfile', 'QosProfileName', 'TimeUnitEnum', 'QosProfileStatusEnum', 'EventId', 'EventType', 'EventTime', 'QosStatusChangedEvent', 'eventDetail', 'NETWORK_TERMINATED', 'StatusInfo', 'ApplicationServer', 'NetworkAccessIdentifier'];
+var separatorsRegex = /\s/     // any whitespace
+var mistakes = [];
 
-// List of exceptions (words that should not be considered as spelling mistakes)
-const exceptions = [
-  'API','MACE','eventId', 'eventType', 'eventTime', 'eventSubscriptionId', 'publicAddress', 
-  'subnet', 'privateAddress', 'publicPort', 'sessionId', 'UUID', 'devicePorts', 'QoS', 'qosProfile', 
-  'TCP', 'UDP', 'QOS_S', 'QOS_M', 'QOS_L', 'QOS_E', 'webhook', 'notificationUrl', 'notificationAuthToken', 
-  'startedAt', 'expiresAt', 'qosprofiles', 'minDuration', 'maxDuration', 'packetDelayBudget', 'oneway', 
-  'endtoend', 'jitter', 'roundtrip', 'ITU', 'eg', 'realtime', 'packetErrorLossRate', 'QCI', 'maxDownstreamRate', 
-  'QOS_STATUS_CHANGED', 'qosStatus', 'statusInfo', 'DURATION_EXPIRED', 'Enduser', 'IoT', 'sensorsactuators', 
-  'phoneNumber', 'networkAccessIdentifier', 'MNO', 'invoker', 'MNOs', 'MSISDN', 'GPSI', 'IdentifierDomain', 
-  'DNS', 'ie', 'applicationServerPorts', 'maxDownstreamBurstRate', 'maxUpstreamRate', 'QoD', 'cmunication', 
-  'QualityOnDemand', 'Telco', 'indepth', 'Telecom', 'VRGaming', 'backend',
-  'OverviewhttpsrawgithubusercontentcomcamaraprojectQualityOnDemandmaindocumentationAPI_documentationresourcesQoD_latency_overviewPNG', 'QOD', 
-  'OAuth', 'andor', 'AppFlow', 'portranges', 'AppFlows', 'portportranges', 'Appflow', 'br', 
-  'APIhttpsrawgithubusercontentcomcamaraprojectQualityOnDemandmaindocumentationAPI_documentationresourcesQoD_detailsPNG', 
-  'CAMARA', 'DRAFThttpsgithubcomcamaraprojectQualityOnDemandblobmaindocumentationAPI_documentationQoSProfile_Mapping_Tablemd', 
-  'IETF', 'addressmask', 'applicationServer', 'dottedquad', 'sessionssessionId', 'createSession', 'targetMinUpstreamRate', 
-  'SessionId', 'SessionInfo', 'EventNotification', 'PhoneNumber', 'QosStatus', 'EventQosStatus', 'ErrorInfo', 'GBR', 'latencysensitive', 
-  'DOCSIS', 'maxUpstreamBurstRate', 'targetMinDownstreamRate', 'qosprofilesname', 'RateUnitEnum', 'CreateSession', 'PortsSpec', 'QosProfile', 
-  'QosProfileName', 'TimeUnitEnum', 'QosProfileStatusEnum', 'EventId', 'EventType', 'EventTime', 'QosStatusChangedEvent', 'eventDetail', 
-  'NETWORK_TERMINATED', 'StatusInfo', 'ApplicationServer', 'NetworkAccessIdentifier'];
-
-// Regular expression to split text into words (space, newline, etc.)
-const separatorsRegex = /\s+/;
-
-// Function to check if a word includes a number
-function includesNumber(word) {
-  return /\d/.test(word);
+function includesNumber(value) {
+    return  /\d/.test(value);
 }
-
-// Function to find spelling mistakes in a file
-function findSpellingMistakesInFile(filePath) {
-  try {
-    // Read the content of the file
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    
-    // Initialize the dictionary
-    dictionary((err, dict) => {
+export default (input) =>{
+    dictionary(ondictionary);
+    function ondictionary(err, dict) {
       if (err) {
-        throw err;
+        throw err
       }
+        var spell = nspell(dict)
+        var no_special_characters= input.replace(/[^\w\s]/gi, '')
+        const words = no_special_characters.split(separatorsRegex);
 
-      // Initialize the spell checker
-      const spell = nspell(dict);
+        var errors= words
+          .filter((word) => !exceptions.includes(word))
+          .filter((word) => !spell.correct(word))
+          .filter((word) => word!='')
+          .filter((word) => includesNumber(word));
 
-      // Split the content into words
-      const words = fileContent.split(separatorsRegex);
-
-      // Filter out exceptions and spelling mistakes
-      const mistakes = words
-        .filter((word) => !exceptions.includes(word.toLowerCase()))
-        .filter((word) => !spell.correct(word))
-        .filter((word) => word !== '')
-        .filter((word) => !includesNumber(word));
-
-      if (mistakes.length > 0) {
-        console.log('Spelling mistakes found:');
-        console.log(mistakes);
-      } else {
-        console.log('No spelling mistakes found.');
-      }
-    });
-  } catch (error) {
-    console.error('An error occurred:', error.message);
-  }
-}
-
-// Example usage:
-const filePath = 'code/API_definitions/qod-api.yaml';
-findSpellingMistakesInFile(filePath);
+        if (errors.length > 0) {
+            mistakes.push(errors);
+        }
+    }
+    console.log(mistakes.length);
+    if (mistakes.length > 0){
+        return [{ message: `Spelling mistakes found: ${mistakes.join(', ')}`,}];
+    }
+};
