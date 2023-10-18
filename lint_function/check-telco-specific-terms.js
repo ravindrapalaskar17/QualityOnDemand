339ea4947296f1b4cc38d70db020aa1b4bbcc2da
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-18 18:49:08 +0530

    Update check-telco-specific-terms.js

--- lint_function/check-telco-specific-terms.js
+++ lint_function/check-telco-specific-terms.js
@@ -1,1 +1,33 @@
 // Write java script function 
+// spectral-functions.js
+const { createSuggestion } = require("@stoplight/spectral-functions");
+
+module.exports = {
+  suggestInclusiveTerms: (target, inclusiveTerms, replacements) => {
+    const messages = [];
+
+    replacements.forEach((replacement) => {
+      const original = replacement.original;
+      const recommended = replacement.recommended;
+
+      const regex = new RegExp(replaceAll(original, /[.*+?^${}()|[\]\\]/g, "\\$&"), "g");
+
+      target.replace(regex, (match, offset) => {
+        messages.push(
+          createSuggestion(`Consider replacing '${original}' with '${recommended}'.`, {
+            range: {
+              start: offset,
+              end: offset + original.length,
+            },
+          })
+        );
+      });
+    });
+
+    return messages;
+  },
+};
+
+function replaceAll(string, search, replace) {
+  return string.split(search).join(replace);
+}
