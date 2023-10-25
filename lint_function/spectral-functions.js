a863aa32bb256285fbf115edf71fcbdeba1eb73c
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-25 17:58:51 +0530

    Delete lint_function/spectral-functions.js

--- lint_function/spectral-functions.js
+++
@@ -1,8 +0,0 @@
-// Import your functions from separate files
-const checkSpellingCode = require('./check-spelling-code.js');
-const checkTelcoSpecificTerms = require('./check-telco-specific-terms.js');
-
-module.exports = {
-  checkSpellingCode,
-  checkTelcoSpecificTerms,
-};
