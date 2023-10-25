9e148d44722cf1fa665517fa7ef96ad490cc4edb
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-25 15:51:33 +0530

    Change function code as per VJ

--- lint_function/check-telco-specific-terms.js
+++ lint_function/check-telco-specific-terms.js
@@ -1,4 +1,4 @@
-var replacements = [
+const replacements = [
   { original: 'UE', recommended: 'device' },
   { original: 'MSISDN', recommended: 'phone number' },
   { original: 'mobile network', recommended: 'network' }
@@ -12,33 +12,32 @@ export default async function (input) {
   const errors = [];
   const suggestions = [];
 
-  // Check the description text for specified terms
+  // Iterate over properties of the input object
   for (const path in input) {
-    const descriptions = JSON.stringify(input[path]).match(/description":\s*"(.*?)"/g) || [];
+    const value = input[path];
 
-    descriptions.forEach((description) => {
+    // Check if the value is a string
+    if (typeof value === 'string') {
       for (const replacement of replacements) {
         const original = replacement.original;
         const recommended = replacement.recommended;
 
-        if (description.includes(original)) {
+        // Use a regular expression to match 'UE' as a standalone word
+        const regex = new RegExp(`\\b${original}\\b`, 'g');
+
+        // Check if the original word exists in the value
+        if (regex.test(value)) {
           errors.push(replacement);
+          suggestions.push(`Consider replacing '${original}' with '${recommended}'.`);
         }
       }
-    });
+    }
   }
 
-  if (errors.length > 0) {
-    errors.forEach((error) => {
-      suggestions.push(`Consider replacing '${error.original}' with '${error.recommended}'.`);
-    });
+  // Check if 'mobile network' is in the suggestions
+  const foundMobileNetwork = suggestions.some((suggestion) => suggestion.includes('mobile network'));
 
-    return [
-      {
-        console.log("Telco-specific terminology found in descriptions: " + suggestions.join(', ')),
-      },
-    ];
+  if (foundMobileNetwork) {
+    console.log('Hint: Telco-specific terminology found in input: ' + suggestions.join(', '));
   }
-
-  return [];
-}
+};
