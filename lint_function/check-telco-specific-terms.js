78061ce5111a40c37da1c6d1748c0e4c337dc06a
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-26 11:32:24 +0530

    Update check-telco-specific-terms.js

--- lint_function/check-telco-specific-terms.js
+++ lint_function/check-telco-specific-terms.js
@@ -4,10 +4,6 @@ const replacements = [
   { original: 'mobile network', recommended: 'network' }
 ];
 
-function includesNumber(value) {
-  return /\d/.test(value);
-}
-
 export default async function (input) {
   const errors = [];
   const suggestions = [];
@@ -22,10 +18,10 @@ export default async function (input) {
         const original = replacement.original;
         const recommended = replacement.recommended;
 
-        // Use a regular expression to match 'UE' as a standalone word
+        // Use a regular expression to match 'original' as a standalone word
         const regex = new RegExp(`\\b${original}\\b`, 'g');
 
-        // Check if the original word exists in the value
+        // Check if 'original' exists in the value
         if (regex.test(value)) {
           errors.push(replacement);
           suggestions.push(`Consider replacing '${original}' with '${recommended}'.`);
@@ -34,10 +30,8 @@ export default async function (input) {
     }
   }
 
-  // Check if 'mobile network' is in the suggestions
-  const foundMobileNetwork = suggestions.some((suggestion) => suggestion.includes('mobile network'));
-
-  if (foundMobileNetwork) {
+  // Check if any word from 'replacements' is in the suggestions
+  if (errors.length > 0) {
     console.log('Hint: Telco-specific terminology found in input: ' + suggestions.join(', '));
   }
 };
