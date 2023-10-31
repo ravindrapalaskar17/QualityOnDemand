5913eec08540a73df0d125c73ea0f609e962699c
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-31 13:22:43 +0530

    change function code

--- lint_function/camara-reserved-words.js
+++ lint_function/camara-reserved-words.js
@@ -75,30 +75,57 @@ const reservedWords = [
   'volatile',
   'while'
 ];
+
 export default async function (input) {
   const errors = [];
   const suggestions = [];
 
-  // Iterate over properties of the input object
-  for (const path in input) {
-    const value = input[path];
-
-    // Check if the value is a string
-    if (typeof value === 'string') {
-      for (const word of reservedWords) {
-        // Use a regular expression to match 'word' as a standalone word
-        const regex = new RegExp(`\\b${word}\\b`, 'g');
-
-        // Check if 'word' exists in the value
-        if (regex.test(value)) {
-          errors.push(word);
-          suggestions.push(`Consider avoiding the use of reserved word '${word}'.`);
+  // Function to check reserved words in an array of strings
+  function checkReservedWordsInArray(arr) {
+    for (const value of arr) {
+      if (typeof value === 'string') {
+        for (const word of reservedWords) {
+          const regex = new RegExp(`\\b${word}\\b`, 'g');
+          if (regex.test(value)) {
+            errors.push(word);
+            suggestions.push(`Consider avoiding the use of reserved word '${word}'.`);
+          }
         }
       }
     }
   }
 
-  // Check if any reserved words are in the suggestions
+  // Check reserved words in the following locations:
+  checkReservedWordsInArray([input.path, input.operation]);
+
+  if (input.parameters) {
+    for (const param of input.parameters) {
+      checkReservedWordsInArray([param.name]);
+    }
+
+    if (input.requestBody) {
+      checkReservedWordsInArray(Object.keys(input.requestBody));
+    }
+  }
+
+  if (input.responses) {
+    for (const response of Object.values(input.responses)) {
+      checkReservedWordsInArray(Object.keys(response));
+    }
+  }
+
+  if (input.securitySchemes) {
+    checkReservedWordsInArray(Object.keys(input.securitySchemes));
+  }
+
+  if (input.components) {
+    checkReservedWordsInArray(Object.keys(input.components));
+  }
+
+  if (input.operationIds) {
+    checkReservedWordsInArray(input.operationIds);
+  }
+
   if (errors.length > 0) {
     console.log('Hint: Reserved words found in input: ' + suggestions.join(', '));
   }
