3ab04de01936ffe0a7d91fcb202ab9e3a8c5bca0
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-31 19:51:17 +0530

    Modify function code

--- lint_function/camara-reserved-words.js
+++ lint_function/camara-reserved-words.js
@@ -1,5 +1,5 @@
 const reservedWords = [
-  'switch',
+  'abstract',
   // Add other reserved words
 ];
 
@@ -7,39 +7,49 @@ export default async function (input) {
   const errors = [];
   const suggestions = [];
 
-  function checkReservedWordsInPathKeys(path) {
-    if (typeof path === 'string') {
-      for (const word of reservedWords) {
-        const regex = new RegExp(`\\b${word}\\b`, 'g');
-        if (regex.test(path)) {
-          errors.push(word);
-          suggestions.push(`Consider avoiding the use of reserved word '${word}' in path key.`);
-        }
-      }
-    }
-  }
-
-  // Iterate over properties of the input object
-  for (const path in input) {
-    const value = input[path];
-
-    // Check if the value is a string
+  // Function to check for reserved words
+  function checkForReservedWords(value, context) {
     if (typeof value === 'string') {
       for (const word of reservedWords) {
         const regex = new RegExp(`\\b${word}\\b`, 'g');
 
         if (regex.test(value)) {
           errors.push(word);
-          suggestions.push(`Consider avoiding the use of reserved word '${word}'.`);
+          suggestions.push(`Consider avoiding the use of reserved word '${word}' in ${context}.`);
         }
       }
     }
-
-    // Check path for reserved words
-    checkReservedWordsInPathKeys(path);
   }
 
-  // Check if any reserved words are in the suggestions
+  // Iterate over paths in the input object
+  if (input.paths) {
+    for (const path in input.paths) {
+      // Check path key for reserved words
+      checkForReservedWords(path, 'path');
+
+      const pathObject = input.paths[path];
+
+      // Check path parameters
+      if (pathObject.parameters) {
+        for (const param of pathObject.parameters) {
+          checkForReservedWords(param.name, 'path parameter');
+        }
+      }
+
+      // Check operations in the path
+      for (const method in pathObject) {
+        if (method !== 'parameters') {
+          const operation = pathObject[method];
+
+          // Check summary and description for reserved words
+          checkForReservedWords(operation.summary, 'summary');
+          checkForReservedWords(operation.description, 'description');
+        }
+      }
+    }
+  }
+
+  // Check if any reserved words are found
   if (errors.length > 0) {
     console.log('Hint: Reserved words found in input: ' + suggestions.join(', '));
   }
