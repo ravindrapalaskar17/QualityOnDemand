fad94258c33f96d33c863e1a05d2eede4699ee0b
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-31 16:18:04 +0530

    chnage function code

--- lint_function/camara-reserved-words.js
+++ lint_function/camara-reserved-words.js
@@ -1,132 +1,72 @@
-const reservedWords = [
-  'abstract',
-  'apiclient',
-  'apiexception',
-  'apiresponse',
-  'assert',
-  'boolean',
-  'break',
-  'byte',
-  'case',
-  'catch',
-  'char',
-  'class',
-  'configuration',
-  'const',
-  'continue',
-  'default',
-  'do',
-  'double',
-  'else',
-  'enum',
-  'extends',
-  'file',
-  'final',
-  'finally',
-  'float',
-  'for',
-  'goto',
-  'if',
-  'implements',
-  'import',
-  'instanceof',
-  'int',
-  'interface',
-  'list',
-  'localdate',
-  'localreturntype',
-  'localtime',
-  'localvaraccept',
-  'localvaraccepts',
-  'localvarauthnames',
-  'localvarcollectionqueryparams',
-  'localvarcontenttype',
-  'localvarcontenttypes',
-  'localvarcookieparams',
-  'localvarformparams',
-  'localvarheaderparams',
-  'localvarpath',
-  'localvarpostbody',
-  'localvarqueryparams',
-  'long',
-  'native',
-  'new',
-  'null',
-  'object',
-  'offsetdatetime',
-  'package',
-  'private',
-  'protected',
-  'public',
-  'return',
-  'short',
-  'static',
-  'strictfp',
-  'stringutil',
-  'super',
-  'switch',
-  'synchronized',
-  'this',
-  'throw',
-  'throws',
-  'transient',
-  'try',
-  'void',
-  'volatile',
-  'while'
-];
-
-export default async function (input) {
-  const errors = [];
-  const suggestions = [];
-
-  // Function to check reserved words in an array of strings
-  function checkReservedWordsInArray(arr) {
-    for (const value of arr) {
-      if (typeof value === 'string') {
-        for (const word of reservedWords) {
-          const regex = new RegExp(`\\b${word}\\b`, 'g');
-          if (regex.test(value)) {
-            errors.push(word);
-            suggestions.push(`Consider avoiding the use of reserved word '${word}'.`);
-          }
-        }
-      }
-    }
-  }
-
-  // Check reserved words in the following locations:
-  checkReservedWordsInArray([input.path, input.operation]);
-
-  if (input.parameters) {
-    for (const param of input.parameters) {
-      checkReservedWordsInArray([param.name]);
-    }
-
-    if (input.requestBody) {
-      checkReservedWordsInArray(Object.keys(input.requestBody));
-    }
-  }
-
-  if (input.responses) {
-    for (const response of Object.values(input.responses)) {
-      checkReservedWordsInArray(Object.keys(response));
-    }
-  }
-
-  if (input.securitySchemes) {
-    checkReservedWordsInArray(Object.keys(input.securitySchemes));
-  }
-
-  if (input.components) {
-    checkReservedWordsInArray(Object.keys(input.components));
-  }
-
-  if (input.operationIds) {
-    checkReservedWordsInArray(input.operationIds);
-  }
-
-  if (errors.length > 0) {
-    console.log('Hint: Reserved words found in input: ' + suggestions.join(', '));
-  }
-}
+openapi: 3.0.3
+info:
+  title: QoD for Enhanced Communication
+  description: The Quality-On-Demand (QoD) API provides a programmable interface for developers and users.
+paths:
+  /sessions:
+    post:
+      summary: Creates a new session
+      requestBody:
+        content:
+          application/json:
+            schema:
+              $ref: "#/components/schemas/CreateSession"
+        required: true
+      responses:
+        "201":
+          description: Session created
+        "400":
+          description: Bad Request
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/ErrorInfo"
+        "500":
+          description: Internal Server Error
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/ErrorInfo"
+components:
+  schemas:
+    CreateSession:
+      type: object
+      properties:
+        duration:
+          type: integer
+          description: Session duration in seconds.
+        device:
+          type: object
+        applicationServer:
+          type: object
+        qosProfile:
+          $ref: "#/components/schemas/QosProfileName"
+        webhook:
+          type: object
+          required:
+            - notificationUrl
+          properties:
+            notificationUrl:
+              type: string
+            notificationAuthToken:
+              type: string
+        required:
+          - device
+          - applicationServer
+          - qosProfile
+    QosProfileName:
+      type: string
+      example: QCI_1_voice
+    ErrorInfo:
+      type: object
+      properties:
+        status:
+          type: integer
+        code:
+          type: string
+        message:
+          type: string
+        required:
+          - status
+          - code
+          - message
