9b43b9038d9419290a5146694d6fa3fbcf555486
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-10-31 18:22:11 +0530

    modify function code

--- lint_function/camara-reserved-words.js
+++ lint_function/camara-reserved-words.js
@@ -1,5 +1,5 @@
 const reservedWords = [
-  'switch',
+  'abstract',
   // Add other reserved words
 ];
 
@@ -7,32 +7,40 @@ export default async function (input) {
   const errors = [];
   const suggestions = [];
 
-  if (input.paths) {
-    // Iterate over the paths
-    for (const path in input.paths) {
-      // Check for reserved words in the path key
-      checkForReservedWords(path, errors, suggestions);
-
-      const pathObject = input.paths[path];
-      // Continue to check other elements within the pathObject as needed.
+  function checkReservedWordsInPathKeys(path) {
+    if (typeof path === 'string') {
+      for (const word of reservedWords) {
+        const regex = new RegExp(`\\b${word}\\b`, 'g');
+        if (regex.test(path)) {
+          errors.push(word);
+          suggestions.push(`Consider avoiding the use of reserved word '${word}' in path key.`);
+        }
+      }
     }
+  }
+
+  // Iterate over properties of the input object
+  for (const path in input) {
+    const value = input[path];
+
+    // Check if the value is a string
+    if (typeof value === 'string') {
+      for (const word of reservedWords) {
+        const regex = new RegExp(`\\b${word}\\b`, 'g');
+
+        if (regex.test(value)) {
+          errors.push(word);
+          suggestions.push(`Consider avoiding the use of reserved word '${word}'.`);
+        }
+      }
+    }
+
+    // Check path for reserved words
+    checkReservedWordsInPathKeys(path);
   }
 
   // Check if any reserved words are in the suggestions
   if (errors.length > 0) {
     console.log('Hint: Reserved words found in input: ' + suggestions.join(', '));
-  }
-}
-
-function checkForReservedWords(value, errors, suggestions) {
-  if (typeof value === 'string') {
-    for (const word of reservedWords) {
-      const regex = new RegExp(`\\b${word}\\b`, 'g');
-
-      if (regex.test(value)) {
-        errors.push(word);
-        suggestions.push(`Consider avoiding the use of reserved word '${word}'.`);
-      }
-    }
   }
 }
