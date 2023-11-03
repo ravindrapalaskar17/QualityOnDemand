0db7ab285a39765d5962fc281819a12a199e0646
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-11-03 15:49:12 +0530

    Update camara-oas-version.js

--- lint_function/camara-oas-version.js
+++ lint_function/camara-oas-version.js
@@ -1,6 +1,6 @@
 export default async function (input) {
-  const value ="3.0.3";
-  if (!input===value) {
+  const value = 3.0.3;
+  if (!input === value) {
     console.log(`There is OpenAPI version not allowed '${input}'.`);
   } 
 }
