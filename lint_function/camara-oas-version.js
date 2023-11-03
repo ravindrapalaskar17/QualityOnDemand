e566242cadbb5e4332e5b0661283202108446b24
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-11-03 16:11:31 +0530

    chnage massage

--- lint_function/camara-oas-version.js
+++ lint_function/camara-oas-version.js
@@ -1,8 +1,8 @@
 export default async function (input) {
   const value = "3.0.3";
   const inputAsString = input.toString(); // Convert input to a string
-
+ 
   if (inputAsString !== value) {
-    console.log(`There is an OpenAPI version not allowed: '${inputAsString}'.`);
+    console.log(`This OpenAPI version not allowed: '${inputAsString}'. Pleae use '${value}`);
   }
 }
