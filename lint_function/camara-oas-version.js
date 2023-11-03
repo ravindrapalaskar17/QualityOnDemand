9d1e80eadf93efb634e4e17912123d09432cbf7f
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-11-03 12:31:26 +0530

    write function code

--- lint_function/camara-oas-version.js
+++ lint_function/camara-oas-version.js
@@ -1,1 +1,7 @@
-
+export default async function checkOpenAPIVersion(input) {
+  if (input.openapi && input.openapi === '3.0.3') {
+    console.log("OpenAPI version is 3.0.3");
+  } else {
+    console.log("Message: Please use OpenAPI version 3.0.3");
+  }
+}
