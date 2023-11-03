7a190693d343f7dacff0f181ae87f4e475c0c8aa
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-11-03 13:03:16 +0530

    Update camara-oas-version.js

--- lint_function/camara-oas-version.js
+++ lint_function/camara-oas-version.js
@@ -1,7 +1,11 @@
 export default async function (input) {
-  if (input.openapi === '3.0.3') {
-    console.log("OpenAPI version is 3.0.3");
+  if (input.openapi) {
+    if (input.openapi === '3.0.3') {
+      console.log("OpenAPI version is 3.0.3");
+    } else {
+      console.log("Message: Please use OpenAPI version 3.0.3");
+    }
   } else {
-    console.log("Message: Please use OpenAPI version 3.0.3");
+    console.log("Message: OpenAPI version is missing. Please specify OpenAPI version 3.0.3.");
   }
 }
