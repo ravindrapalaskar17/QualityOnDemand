c9c1479d2e4d81bb9365c17b2929649c49db19b7
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-11-07 21:15:37 +0530

    Update camara-language-spelling.js

--- lint_function/camara-language-spelling.js
+++ lint_function/camara-language-spelling.js
@@ -34,37 +34,3 @@ export default async function (input) {
       message: `Spelling mistakes found: ${mistakes.join(', ')}`,
     }];});
 };
- 30 changes: 30 additions & 0 deletions30  
-spell-check.js
-Viewed
-@@ -0,0 +1,30 @@
-const fs = require('fs');
-const spellChecker = require('spellchecker');
-
-function findSpellingMistakesInYamlFile(filePath) {
-  // Read the content of the YAML file
-  const yamlContent = fs.readFileSync(filePath, 'utf8');
-
-  // Split the content into words (split by spaces, newlines, etc.)
-  const words = yamlContent.split(/\s+/);
-
-  // Filter out any words that should be excluded from spell checking
-  const exceptions = ["bic", "datetime", "gt", "gte", "icontains", "iban", "idempotency", "isnull", "lt", "lte", "md5", "mimetype", "oid", "userpic"];
-  const filteredWords = words.filter((word) => !exceptions.includes(word.toLowerCase()));
-
-  // Find spelling mistakes
-  const mistakes = filteredWords.filter((word) => spellChecker.isMisspelled(word));
-
-  return mistakes;
-}
-
-// Example usage:
-const filePath = 'code/API_definitions/qod-api2.yaml';
-const spellingMistakes = findSpellingMistakesInYamlFile(filePath);
-
-if (spellingMistakes.length > 0) {
-  console.log('Spelling mistakes found:');
-  console.log(spellingMistakes);
-} else {
-  console.log('No spelling mistakes found.');
-}
