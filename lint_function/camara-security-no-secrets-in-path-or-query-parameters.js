5fe7c862b96e0fea246777e4c7d0ba3446fe8463
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-11-08 11:59:18 +0530

    Update camara-security-no-secrets-in-path-or-query-parameters.js

--- lint_function/camara-security-no-secrets-in-path-or-query-parameters.js
+++ lint_function/camara-security-no-secrets-in-path-or-query-parameters.js
@@ -1,1 +1,18 @@
+const sensetiveData = ['MSISDN','IMSI'];
 
+export default async function (input) {
+  
+  // Iterate over properties of the input object
+  for (const path in input) {
+    
+    if (typeof path === 'string') {
+      for (const word of sensetiveData) {
+        const regex = new RegExp(`\\b${word}\\b`, 'g');  // Use a regular expression to match 'word' as a standalone word
+    
+           if (regex.test(path)) {
+               console.log(`Warn: Sensetive Data found in input: '${path}' Consider avoiding the use of Sesentive data '${word}'. `);
+        }
+      }
+    }
+  }
+}
