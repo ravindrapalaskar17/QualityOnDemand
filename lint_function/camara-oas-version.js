89b9633aaeead4fce6ebb8678ead97b935b523aa
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-11-20 17:16:43 +0530

    Delete lint_function/camara-oas-version.js

--- lint_function/camara-oas-version.js
+++
@@ -1,8 +0,0 @@
-export default async function (input) {
-  const value = "3.0.3";
-  const inputAsString = input.toString(); // Convert input to a string
- 
-  if (inputAsString !== value) {
-    console.error(`Error:'${inputAsString}' :This OpenAPI version not allowed, Pleae use '${value}'.`);
-  }
-}
