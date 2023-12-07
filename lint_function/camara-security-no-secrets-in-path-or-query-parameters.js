ba79f82915d63ca17ee0cda9b7302722f629d2b0
ravindrapalaskar17 <117813163+ravindrapalaskar17@users.noreply.github.com>
2023-12-07 15:00:42 +0530

    Update camara-security-no-secrets-in-path-or-query-parameters.js

--- lint_function/camara-security-no-secrets-in-path-or-query-parameters.js
+++ lint_function/camara-security-no-secrets-in-path-or-query-parameters.js
@@ -10,7 +10,12 @@ export default async function (input) {
         const regex = new RegExp(`\\b${word}\\b`, 'g');  // Use a regular expression to match 'word' as a standalone word
     
            if (regex.test(path)) {
-               console.log(`Warn: Sensetive Data found in input: '${path}' Consider avoiding the use of Sesentive data '${word}'. `);
+
+              const warningRuleName = 'camara-security-no-secrets-in-path-or-query-parameters';
+              const description = `Sensetive Data found in path: Consider avoiding the use of Sesentive data '${word}'`;
+              const location = `paths.${path}`;
+              console.log(`warning  ${warningRuleName}  ${description}  ${location}`);
+             
         }
       }
     }
